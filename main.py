5ae9ace523ba5ffa2dcc51eaee1c375905b3d93f
Jonathan Artman <230405@student.ccts.org>
2024-05-17 12:25:14 +0000

    first version

--- main.py
+++ main.py
@@ -6,82 +6,49 @@ import tkinter as tk
 import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
+from tkinter import messagebox
 
+window = tk.Tk()
 
-    # Modify the do_command function:
-#   to use the new button as needed
-def do_command(command):
-    global command_textbox
-
-    url_val = url_entry.get()
-    if (len(url_val) == 0):
-        # url_val = "127.0.0.1"
-        url_val = "::1"
-
-    command_textbox.delete(1.0, tk.END)
-    command_textbox.insert(tk.END, command + " working....\n")
-    command_textbox.update()
-
-    p = subprocess.Popen(command + ' ::1', stdout=subprocess.PIPE, stderr=subprocess.PIPE) #v2
-
-    cmd_results, cmd_errors = p.communicate()
-    command_textbox.insert(tk.END, cmd_results)
-    command_textbox.insert(tk.END, cmd_errors)
-
-    
+def do_command():
+    subprocess.call("ping localhost")
 
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
 
-# Modifies the ping button parameters.
-ping_btn = tk.Button(frame, text="Check to see if a URL is up and active", 
-    command=lambda:do_command("ping"),
-    compound="center",
-    font=("comic sans", 12),
-    bd=0, 
-    relief="flat",
-    cursor="heart",
-    bg="lightblue", activebackground="lightblue")
-ping_btn.pack() 
+# set up button to run the do_command function
+blue_frame = tk.Frame(window, bg="orange", width=800, height=800)
+blue_frame.pack(side=tk.LEFT, padx=10, pady=10)
+
+#text box 1
+entry_blue = tk.Entry(blue_frame)
+entry_blue.pack(pady=5)
+
+#drop down
+exchange_rates = {
+    "nslookup",
+    "nmap",
+    "man",
+    }
+
+currency_var = tk.StringVar()
+currency_var.set("Select a tool")
+currency_choice = tk.OptionMenu(window, currency_var, *exchange_rates)
+currency_choice.pack(padx=10, pady=5)
+
+
+# button
+calculation_button = tk.Button(window, text="Convert", command=do_command)
+calculation_button.pack(pady=5)
+    
+#output box
+command_textbox = tksc.ScrolledText(frame, height=5, width=5)
+command_textbox.grid(row=10, column=5)
 
 
 
-frame_URL = tk.Frame(root, pady=10,  bg="lightblue") # change frame color
-frame_URL.pack()
 
-# decorative label
-url_label = tk.Label(frame_URL, text="Enter a URL of interest: ", 
-    compound="center",
-    font=("comic sans", 14),
-    bd=0, 
-    relief=tk.FLAT, 
-    cursor="heart",
-    fg="mediumpurple3",
-    bg="lightblue")
-url_label.pack(side=tk.LEFT)
-url_entry= tk.Entry(frame_URL,  font=("comic sans", 14)) # change font
-url_entry.pack(side=tk.LEFT)
-
-
-
-frame = tk.Frame(root,  bg="lightblue") # change frame color
-frame.pack()
-
-# Adds an output box to GUI.
-command_textbox = tksc.ScrolledText(frame, height=10, width=100)
-command_textbox.pack()
-
-# Save function.
-def mSave():
-  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
-  if filename is None:
-    return
-  file = open (filename, mode = 'w')
-  text_to_save = command_textbox.get("1.0", tk.END)
-  
-  file.write(text_to_save)
-  file.close()
 
 
 
