03d9a36bd47257d8bf6ba5a87f0f2bcfc0e1574f
Alonso Gomez <230077@student.ccts.org>
2024-05-20 12:46:13 +0000

    finished

--- main.py
+++ main.py
@@ -7,9 +7,24 @@ import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
 
-def do_command():
-    subprocess.call("ping localhost")
+def do_command(command):
+    global command_textbox, url_entry
 
+    # If url_entry is blank, use localhost IP address 
+    url_val = url_entry.get()
+    if (len(url_val) == 0):
+        #url_val = "127.0.0.1"
+        url_val = "::1"
+    command_textbox.delete(1.0, tk.END)
+    command_textbox.insert(tk.END, command + " working....\n")
+    command_textbox.update()
+
+    p = subprocess.Popen(command + ' ' + url_val, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True) #v2
+
+    cmd_results, cmd_errors = p.communicate()
+    command_textbox.insert(tk.END, cmd_results)
+    command_textbox.insert(tk.END, cmd_errors)
+   
 root = tk.Tk()
 frame = tk.Frame(root)
 frame.pack()
@@ -20,8 +35,16 @@ command_textbox = tksc.ScrolledText(frame, height=10, width=100)
 command_textbox.pack()
 
 # set up button to run the do_command function
-ping_btn = tk.Button(frame, text="ping", command=do_command)
+ping_btn = tk.Button(frame, text="ping", command=lambda:do_command("ping -c5"))
 ping_btn.pack()
+
+
+nslook_btn = tk.Button(frame, text="nslookup", command=lambda:do_command("nslookup"))
+nslook_btn.pack()
+
+nmap_btn = tk.Button(frame, text="nmap", command=lambda:do_command("nmap"))
+nmap_btn.pack()
+
 # creates the frame with label for the text box
 frame_URL = tk.Frame(root, pady=10,  bg="black") # change frame color
 frame_URL.pack()
@@ -41,6 +64,21 @@ url_entry.pack(side=tk.LEFT)
 
 frame = tk.Frame(root,  bg="black") # change frame color
 frame.pack()
+
+
+def mSave():
+  filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+  if filename is None:
+    return
+  file = open (filename, mode = 'w')
+  text_to_save = command_textbox.get("1.0", tk.END)
+  
+  file.write(text_to_save)
+  file.close()
+
+save_btn = tk.Button(frame, text="save", command=lambda:mSave())
+save_btn.pack()
+
 root.mainloop()
 
 
