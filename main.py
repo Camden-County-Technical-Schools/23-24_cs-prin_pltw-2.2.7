8912eb1e18f8ab6c09da5c9f5c32aca33ff67241
Andrew Kuzniasz <aekuzniasz@gmail.com>
2024-05-20 12:49:00 +0000

    aqswderftgyhjuiklop;['] ']

--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import subprocess
 import tkinter as tk
+from tkinter import *
 import tkinter.scrolledtext as tksc
 from tkinter import filedialog
 from tkinter.filedialog import asksaveasfilename
@@ -42,9 +43,9 @@ url_entry.pack(side=tk.LEFT)
 
 
 #default off values
-ping_selected=0
-nmap_selected=0
-nslookup_selected=0
+ping_selected = IntVar()
+nmap_selected = IntVar()
+nslookup_selected = IntVar()
 
 #checkboxes themselves
 #ping
@@ -52,18 +53,41 @@ ping_check = tk.Checkbutton(frame, text='ping',variable=ping_selected, onvalue=1
 ping_check.grid(row=4,column=1)
 
 #nmap
-nmap_check = tk.Checkbutton(frame, text='nmap',variable=nmap_selected, onvalue=3, offvalue=2)
+nmap_check = tk.Checkbutton(frame, text='nmap',variable=nmap_selected, onvalue=1, offvalue=0)
 nmap_check.grid(row=5,column=1)
 #nslookup
-nslookup_check = tk.Checkbutton(frame, text='nslookup',variable=nslookup_selected, onvalue=5, offvalue=4)
+nslookup_check = tk.Checkbutton(frame, text='nslookup',variable=nslookup_selected, onvalue=1, offvalue=0)
 nslookup_check.grid(row=6,column=1)
 
 
 ######################################################code for doing the selected command.
 def do_command():
     command = "Errrmmm what the sigma.  chat is this real?"
-    if ping_selected == 1:
+    if ping_selected.get() == 1:
         command="ping -c5"
+    else:
+        if nmap_selected.get() == 1:
+            command="nmap"
+        else:
+            if nslookup_selected.get() == 1:
+                command="nslookup"
+
+    # Modify the do_command(command) function: 
+    #   to use the text box for input to the functions
+    global command_textbox, url_entry
+
+    # If url_entry is blank, use localhost IP address 
+    url_val = url_entry.get()
+    if (len(url_val) == 0):
+        # url_val = "127.0.0.1"
+        url_val = "::1"
+    
+        # use url_val 
+        p = subprocess.Popen(command + ' ::1', stdout=subprocess.PIPE, stderr=subprocess.PIPE) #v2
+
+
+
+
 
     T.delete(1.0, tk.END)
     T.insert(tk.END, command + " working....\n")
@@ -84,7 +108,7 @@ proceed_btn.grid(row=7,column=1)
 ##################################################Text widget where output will go
 # Create text widget and specify size.
 global T
-T = tksc.ScrolledText(frame, height = 10, width = 40)
+T = tksc.ScrolledText(frame, height = 10, width = 70)
 
 T.grid(row=5,column=0)
 
@@ -92,8 +116,21 @@ T.insert(tk.END, "output")
 
 
 
-######### set up button to run the do_command function
-proceed_btn = tk.Button(frame, text="Save output to file", command=do_command)
+######### set up button to run the save_output function
+def save_output():
+    filename = asksaveasfilename(defaultextension='.txt',filetypes = (('Text files', '*.txt'),('Python files', '*.py *.pyw'),('All files', '*.*')))
+    if filename is None:
+        return
+    file = open (filename, mode = 'w')
+    text_to_save = T.get("1.0", tk.END)
+
+    file.write(text_to_save)
+    file.close()
+
+
+
+
+proceed_btn = tk.Button(frame, text="Save output to file", command=save_output)
 proceed_btn.grid(row=8,column=1)
 
 
